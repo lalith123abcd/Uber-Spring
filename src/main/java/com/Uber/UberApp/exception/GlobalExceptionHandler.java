b8a03c2dd66b7ad76f4f9a257d44a59f613df21e
lalith123abcd <lalithkumar97979@gmail.com>
2025-11-06 01:38:03 +0530

    Added GlobalExceptionHandler

--- src/main/java/com/Uber/UberApp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Uber/UberApp/exception/GlobalExceptionHandler.java
@@ -1,12 +1,21 @@
 package com.Uber.UberApp.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    public ResponseEntity<Map<String,String>> handleIllegalArgumentException
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String,String>> handleIllegalArgumentException(IllegalArgumentException e){
+        Map<String,String> error=new HashMap<>();
+        error.put("message",e.getMessage());
+        error.put("status", HttpStatus.BAD_REQUEST.toString());
+        return ResponseEntity.badRequest().body(error);
+    }
 }
